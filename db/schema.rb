7d87821b719cec78d9762577ce17a943e5c7affc
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-05-25 16:30:38 +0200

    add url for item picture to equipment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_21_135727) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_142841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_21_135727) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "item_url"
+    t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
