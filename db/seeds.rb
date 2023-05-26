8038c812520e4b209469acc044f74cee634859e5
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-05-26 15:40:58 +0200

    add farm equipment data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,7 @@ location = [
 ]
 
 # category = [
-#   "garden",
+#
 #   "lawn",
 #   "farm",
 #   "power tools",
@@ -143,7 +143,7 @@ Equipment.create(
   description: "A shovel is the number one tool in most gardeners’ sheds, and
   it’s probably the most versatile. Shovels are common tools that are used
   extensively in agriculture, construction and gardening. It is used for
-  collecting a lifting."
+  collecting a lifting.",
 )
 
 Equipment.create(
@@ -155,7 +155,88 @@ Equipment.create(
   rented: 0,
   user_id: rand(1..5),
   item_url: "xajyv4pkv8lfytec9h2u", # Replace with actual image URL
-  description: "Trimming lawn edges, Ideal for medium to larger gardens."
+  description: "Trimming lawn edges, Ideal for medium to larger gardens.",
+)
+
+# Equipment data - Farm
+
+Equipment.create(
+  category: "farm",
+  name: "Auger / Two man earth drill",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "tmzqtdj12ps6kvca3wzh", # Replace with actual image URL
+  description: "Drilling holes in ground for planting wooden or concrete posts
+  or poles.  Used in agriculture for planting wine posts, fencing posts or tree
+  planting.",
+)
+
+Equipment.create(
+  category: "farm",
+  name: "Pick",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "dsdchjrzzup1qtp6fw4h", # Replace with actual image URL
+  description: "The pointed edge is most often used to break up rocky surfaces
+  or other hard surfaces such as concrete or hardened dried earth. The large
+  momentum of a heavy pickaxe, combined with the small contact area, makes it
+  very effective for this purpose. The chiseled end, if present, is used for
+  purposes including cutting through roots",
+)
+
+Equipment.create(
+  category: "farm",
+  name: "Killa Spray",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "lshduxnxi3mei3lspz9i", # Replace with actual image URL
+  description: "To spray and kill insects. Used to spray retarding agents on
+  concrete.",
+)
+
+Equipment.create(
+  category: "farm",
+  name: "Chain Saw 2-Stroke",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "dhkie8qz1dkdfsm2obsd", # Replace with actual image URL
+  description: "Ideal for cutting down small to medium sized trees and trimming thick branches.",
+)
+
+Equipment.create(
+  category: "farm",
+  name: "Blower (Petrol)",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "qosfhtbhevi2pjxn6zgd", # Replace with actual image URL
+  description: "Used in Garden Services for blowing of leaves and grass trimmings. Also handy on construction sites for dust removal.",
+)
+
+Equipment.create(
+  category: "farm",
+  name: "Plough / Tiller / Rotavator (Diesel)",
+  location: location.sample,
+  quantity: rand(1..3),
+  price: Faker::Number.between(from: 10.00, to: 1_000_000),
+  rented: 0,
+  user_id: rand(1..5),
+  item_url: "l08lawkoqbyo1q5ghov7", # Replace with actual image URL
+  description: "Ploughing of small area soil or between vineyards. Also used to soften soil for digging long trenches.",
 )
 
 puts "#{Equipment.all.count} equipment items created"
