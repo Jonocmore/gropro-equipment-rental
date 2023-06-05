38d3827edf6d489a4a1160155f581196b1032a76
Percy Manenzhe <percy@ptpi.co.za>
2023-06-05 11:18:00 +0200

    updated_rental_calendar

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :equipment
+
+  def duration
+    return unless rented_date.present? && return_date.present?
+
+    return_date.to_date - rented_date.to_date
+  end
+
+  def total_cost
+    duration.to_i * equipment.price
+  end
 end
